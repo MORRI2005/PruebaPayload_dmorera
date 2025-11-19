e5999eef3524a9e1c583a2265545ae8d43633999
David Morera Mondejar <david.morera0804@gmail.com>
2025-11-19 09:04:52 +0100

    feat: "Add ButtonBlock and ButtonComponent"

--- src/app/(frontend)/blockcomps/ContentBlock.tsx
+++ src/app/(frontend)/blockcomps/ContentBlock.tsx
@@ -1,6 +1,7 @@
 import { Page } from '@/payload-types'
 import { RichText } from '@payloadcms/richtext-lexical/react'
 import React from 'react'
+import ButtonComponent from '@/app/(frontend)/blockcomps/compoBlocks/ButtonComponent'
 
 type ContentProps = Extract<Page['layout'][0], { blockType: 'content' }>
 
@@ -9,6 +10,14 @@ export default function ContentBlock({ block }: { block: ContentProps }) {
     <div id="content_block">
       <h2>{block.title}</h2>
       <RichText data={block.content} />
+      {block.compos?.map((innerBlock, i) => {
+        switch (innerBlock.blockType) {
+          case 'button':
+            return <ButtonComponent key={i} block={innerBlock}></ButtonComponent>
+          default:
+            return null
+        }
+      })}
     </div>
   )
 }
---
+++ src/app/(frontend)/blockcomps/compoBlocks/ButtonComponent.tsx
@@ -0,0 +1,19 @@
+import { Page } from '@/payload-types'
+import React from 'react'
+
+// type ButtonBlockType = Extract<Page['layout'][0]['compos'][0], {blockType: 'button'}>
+
+type ButtonBlockType = {
+  blockType: 'button'
+  text: string
+  url?: string
+  style: 'primary' | 'secondary'
+}
+
+export default function ButtonComponent({ block }: { block: ButtonBlockType }) {
+  return (
+    <a href={block.url} className="mi-btn">
+      {block.text}
+    </a>
+  )
+}
